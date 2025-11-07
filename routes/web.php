1862e9d4d51c08c925ab01d86b9b594ce53f0030
jrmvlljs0 <jrmvlljs@aiowireless.ph>
2025-11-07 14:21:55 +0800

    Commit
    Push email verification and forgot password

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // Stock/Prodcut routes for managing product stock movements 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','verified')->group(function () {
     Route::resource('products', ProductController::class)->middleware('auth');
     Route::get('/stock', [ProductStockController::class, 'index'])->name('stock.index');
     Route::get('/stock/create', [ProductStockController::class, 'create'])->name('stock.create');
@@ -35,3 +39,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+ 
